1962ca8831280176e968c974996784ec9558fefb
Alberto Harka <alb.harka@icloud.com>
2024-09-25 18:58:50 +0200

    Created command line configuration with yargs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,48 @@
-import { args } from "./lib/args.ts";
+import yargs from "yargs";
 import { run } from "./lib/runner.ts";
+import { hideBin } from "yargs/helpers";
 
-run(async () => {
-  const { csvPath } = args();
-  console.log(csvPath);
-}, {
-  onError: (err) => {
-    console.error(err.message);
-    process.exit(1);
-  },
-});
+const cli = yargs(hideBin(process.argv))
+	.version("0.0.1")
+	.usage("Usage: $0 start <csvPath> [options]")
+	.option("delimiter", {
+		alias: "d",
+		string: true,
+		default: ",",
+	})
+	.help("help")
+	.check((args) => {
+		const paths = args._;
+		if (paths.length === 0) {
+			throw new Error("File path is required");
+		}
+
+		if (paths.length > 1) {
+			throw new Error("Only one file path is allowed");
+		}
+
+		if (typeof paths[0] === "string" && paths[0].endsWith(".csv")) {
+			throw new Error("File path must not end with .csv");
+		}
+
+		if (args.delimiter.length !== 1) {
+			throw new Error("Delimiter must be a single character");
+		}
+		return true;
+	});
+
+run(
+	async () => {
+		const { delimiter, _: paths } = await cli.parse();
+		// we are sure it's a string because of yarg check
+		const csvPath = paths[0] as string;
+
+		console.log(csvPath, delimiter);
+	},
+	{
+		onError: (err) => {
+			console.error(err.message);
+			process.exit(1);
+		},
+	},
+);
